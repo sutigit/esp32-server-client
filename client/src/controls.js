b2205b3429a7c022f3046c82058ff7de7d6b3491
sutigit <panyanilben@gmail.com>
2025-05-30 23:02:50 +0300

    init wifi setup

--- client/src/controls.js
+++ client/src/controls.js
@@ -5,9 +5,7 @@ dotenv.config();
 export const serverURL = `http://${process.env.SERVER_IP}:${process.env.SERVER_PORT}`;
 
 export const blinkPin = async (pin) => {
-  const esp32SafePins = [
-    2, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 25, 26, 27, 32, 33,
-  ];
+  const esp32SafePins = [14, 25, 26, 27, 32, 33, 34, 35, 37, 38, 39];
 
   if (!esp32SafePins.includes(pin)) {
     console.error(`Pin ${pin} is not a safe pin for blinking.`);
--- esp32/blink/src/main.cpp
+++ esp32/blink/src/main.cpp
@@ -1,14 +1,46 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
-#define LED_PIN 2  // On many ESP32 boards, GPIO 2 is the built-in LED
+WebServer server(80); // Port 80
 
-void setup() {
-  pinMode(LED_PIN, OUTPUT);
+const char* ssid = "sutilan";
+const char* password = "leppakerttu";
+
+const int ledPins[11] = {14,25,26,27,32,33,34,35,37,38,39};
+const int numPins = sizeof(ledPins) / sizeof(ledPins[0]);
+
+void blink() {
+  Serial.println("blink");
 }
 
-void loop() {
-  digitalWrite(LED_PIN, HIGH);  // Turn LED on
-  delay(1000);                  // Wait 1 second
-  digitalWrite(LED_PIN, LOW);   // Turn LED off
-  delay(1000);                  // Wait 1 second
+void setup(){
+    Serial.begin(460800);
+    delay(1000);
+
+    WiFi.mode(WIFI_STA); // The STA mode is used to connect the ESP32 to a Wi-Fi network, provided by an Access Point.
+    WiFi.begin(ssid, password);
+    Serial.println("\nConnecting");
+
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(100);
+    }
+
+    Serial.println("\nConnected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.println("\nInitializing LED pins");
+    for (int i = 0; i < numPins; i++) {
+      pinMode(ledPins[i], OUTPUT);
+    }
+
+    server.on("/", blink);
+
+    server.begin();
+    Serial.println("\nHTTP server started");
+}
+
+void loop(){
+  server.handleClient();
 }
